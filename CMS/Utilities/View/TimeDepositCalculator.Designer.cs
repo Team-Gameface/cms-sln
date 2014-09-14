88e02be05d3bcd55208b83d69252fcb30785cb43
Levin Calado <levincalado@gmail.com>
2014-09-14 11:22:19 +0800

    Initialized Utilities View Classes

---
+++ CMS/Utilities/View/TimeDepositCalculator.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace CMS.Utilities.View
+{
+    partial class TimeDepositCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TimeDepositCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 341);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "TimeDepositCalculator";
+            this.Text = "Time Deposit Calculator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
